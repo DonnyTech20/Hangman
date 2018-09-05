62b16f0fcb957ab4d8a726e0192becdd48b59c45
Donny Techiera <DonnyTech20@gmail.com>
2018-09-05 10:20:42 -0400

    code added

---
+++ hangman.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint;          // Word getHint
+    var word;              // Selected word
+    var guess;             // Geuss
+    var geusses = [];      // Stored geusses
+    var lives;             // Lives
+    var counter;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
+
+    // Get elements
+    var showLives = document.getElementById("mylives");
+    var showCatagory = document.getElementById("catagoryName");
+    var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+
+
+
+    // create alphabet ul
+    var buttons = function () {
+        myButtons = document.getElementById('buttons');
+        letters = document.createElement('ul');
+
+        for (var i = 0; i < alphabet.length; i++) {
+            letters.id = 'alphabet';
+            list = document.createElement('li');
+            list.id = 'letter';
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    }
+
+
+    // Select Catagory
