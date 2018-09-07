ae830795238d69600588ebf5e81d50a27c013a18
Donny Techiera <DonnyTech20@gmail.com>
2018-09-07 10:26:22 -0400

    added code

--- hangman.js
+++ hangman.js
@@ -4,15 +4,6 @@ window.onload = function () {
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    // var categories;         // Array of topics
-    //var chosenCategory;     // Selected catagory
-    //var getHint;          // Word getHint
-    //var word;              // Selected word
-    //var guess;             // Geuss
-    //var geusses = [];      // Stored geusses
-    //var lives;             // Lives
-    //var counter;           // Count correct geusses
-    //var space;              // Number of spaces in word '-'
 
     // Get elements
     var showLives = document.getElementById("mylives");
